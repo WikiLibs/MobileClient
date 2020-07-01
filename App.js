32abcc3c1ef28127a2490370ff6259e08c70f6ab
Samuel <samuel.rousseaux@epitech.eu>
2020-07-01 18:24:47 +0900

    Moved details component into test

--- App.js
+++ App.js
@@ -4,28 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from './components/Home'
-
-function DetailsScreen({ route, navigation }) {
-  const { itemId } = route.params;
-  const { otherParam } = route.params;
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Text>itemId: {JSON.stringify(itemId)}</Text>
-      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() =>
-          navigation.push('Details', {
-            itemId: Math.floor(Math.random() * 100),
-          })
-        }
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-    </View>
-  );
-}
+import DetailsScreen from './components/DetailsTest'
 
 const Stack = createStackNavigator();
 
--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { Text, View, Button } from 'react-native';
 
 export default function Home({ navigation }) {
   return (
